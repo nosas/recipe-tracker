f60419d33c3527d79b4dc19b94abaf34fe44bffa
Sason <sasonreza@gmail.com>
2023-12-21 23:18:55 -0800

    Create simple Dash page for inserting a dish into the test database

---
+++ src/tracker/app.py
@@ -0,0 +1,36 @@
+import sys
+
+import dash
+from dash import Dash, dcc, html
+
+sys.path.append("../src")
+from database.utils.connection import RecipeDBAccess
+
+app = Dash(__name__, use_pages=True)
+app.layout = html.Div(
+    [
+        html.H1("Multi-page app with Dash Pages"),
+        html.Div(
+            [
+                html.Div(
+                    dcc.Link(
+                        f"{page['name']} - {page['path']}", href=page["relative_path"]
+                    )
+                )
+                for page in dash.page_registry.values()
+            ]
+        ),
+        dash.page_container,
+    ]
+)
+
+
+if __name__ == "__main__":
+    db = RecipeDBAccess.get_instance(
+        username="test_user",
+        password="postgresql",
+        prod_db=False,
+    )
+    db.create_tables()
+    app.run_server(debug=True)
+    db.drop_tables(force=True)
---
+++ src/tracker/pages/add_dish.py
@@ -0,0 +1,40 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+
+from database.schema.models import Dish
+from database.utils.connection import RecipeDBAccess
+
+dash.register_page(__name__)
+
+
+def layout():
+    return html.Div(
+        [
+            dcc.Input(id="dish-name", type="text", placeholder="Enter dish name"),
+            html.Button("Submit", id="submit-button", n_clicks=0),
+            html.Div(id="output-state"),
+        ]
+    )
+
+
+@dash.callback(
+    Output("output-state", "children"),
+    [Input("submit-button", "n_clicks")],
+    [State("dish-name", "value")],
+)
+def update_output(n_clicks, dish_name):
+    if n_clicks > 0:
+        # Connect to the database
+        db = RecipeDBAccess.get_instance(
+            username="test_user",
+            password="postgresql",
+            prod_db=False,
+        )
+
+        # Insert the new dish
+        dish = Dish(name=dish_name)
+        db.insert_one(dish)
+
+        return f"Dish {dish_name} added successfully!"
+    return ""
